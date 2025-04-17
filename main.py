d9f487f436f6f0dc02be24b984648c638769256b
mrx <meibrohimov@gmail.com>
2025-04-17 11:02:56 +0500

    response conf

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.commit()
 
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
